54c3e38668b27d197284960cc62b52c671940e1c
kjh-appro <57822259+kjh-appro@users.noreply.github.com>
2021-03-26 01:32:16 +0900

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "gatsby";
 import { ThemeToggler } from "gatsby-plugin-dark-mode";
-import { HeadLogo } from “.../header-logo.png”;
+import { HeadLogo } from "../header-logo.png";
 
 
 class Layout extends Component {
@@ -40,7 +40,7 @@ class Layout extends Component {
             className="header-title"
             to={`/`}
           >
-        <img src="./image/header-logo.png" alt="fireSpot"/>
+        <img src="../header-logo.png" alt="fireSpot"/>
           <div className="nav-container">
             <ul className="header-nav">
               <li id="header-nav-first"><Link to={`/tags`}>Tags</Link></li>
