b479d4d85b948eb5e71e339113058034bf08a729
kjh-appro <57822259+kjh-appro@users.noreply.github.com>
2021-03-31 20:59:58 +0900

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,8 +29,8 @@ class Layout extends Component {
       <div className="header-menu-bar">
         <div className="nav-container">
             <ul className="header-nav">
-            <li><Link to={`/`}>Home</Link></li>
-              <li id="header-nav-first"><Link to={`/tags`}>Tags</Link></li>
+              <li id="header-nav-first"><Link  to={`/`}>Home</Link></li>
+              <li><Link to={`/tags`}>Tag</Link></li>
               <li><Link to={`/search`}>Search</Link></li>
               <li>{toggler}</li>
             </ul>
