1a52a9bac6ff9785b1ad88a1d14606cd3e1a0920
kjh-appro <57822259+kjh-appro@users.noreply.github.com>
2021-04-03 20:04:40 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,6 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `static/favicon.ico`,
       },
     },
     `gatsby-plugin-offline`,
